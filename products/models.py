e33a1dc3e64fc492864f618a730a63d9032702c7
Lakshmivenkateshwaran <vvenkivasudevan9@gmail.com>
2026-05-14 10:52:30 +0530

    Initial Django project

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    price = models.IntegerField()
+
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
---
+++ products/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+
+# Get method
+@api_view(['GET', 'POST'])
+def products(request):
+
+    # GET API
+    if request.method == 'GET':
+
+        products = Product.objects.all()
+
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+    
+    # POST API
+    elif request.method == 'POST':
+
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
